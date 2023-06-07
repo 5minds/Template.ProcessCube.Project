5378bc8efd2aeeee6b47d79e737cf297af81161e
Marius Jahn <Marius.jahn@5minds.de>
2023-06-07 12:53:17 +0200

    add some tailwind stuff

--- Template.ProcessCube.Project/apps/Template.ProcessCube.Project/frontend/app/ExampleCustomForm/ExampleCustomForm.tsx
+++ Template.ProcessCube.Project/apps/Template.ProcessCube.Project/frontend/app/ExampleCustomForm/ExampleCustomForm.tsx
@@ -8,12 +8,13 @@ const ExampleCustomForm = (props: CustomFormProps) => {
 
   return (
     <>
+      <p className='text-heading-2 text-5mindsorange'>5<span className='text-heading-2 text-5mindsblue'>Minds</span></p>
       <p className='text-heading-1'>Heading 1</p>
       <p className='text-heading-2'>Heading 2</p>
       <p className='text-heading-3'>Heading 3</p>
 
-      <button className='mt-8 p-4 bg-lime-200 hover:bg-lime-300 active:bg-red-300 rounded-md ' onClick={() => props.finishUserTask({})}>
-        Confirm
+      <button className='mt-8 p-4 bg-lime-200 hover:bg-lime-300 active:bg-red-300 rounded-md drop-shadow-lg active:drop-shadow-none' onClick={() => props.finishUserTask({})}>
+        Task beenden
       </button>
     </>
   )
